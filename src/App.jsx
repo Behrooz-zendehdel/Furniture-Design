56d9e8eb03be63b55b61c4bda89a25576511e09b
behrooz <behrooz.zendedel1992@gmail.com>
2023-11-19 22:10:17 +0330

    added assets and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-function App () {
-  return(
-      <>
-        <Hero />
-        <Services />
-        <Product />
-        <Refrence />
-        <Care />
-        <Footer />
-      </>
-  )
-
+import Hero from "./pages/Hero.jsx";
+import Services from "./pages/Services.jsx";
+import Product from './pages/Products.jsx'
+import Refrence from './pages/Refrence.jsx'
+import Care from './pages/Care.jsx'
+import Footer from './pages/Footer.jsx'
+function App() {
+  return (
+    <>
+       {/* added structure design  step 1 - 6 */}
+    
+      <Hero />
+      {/* <Services /> */}
+      {/* <Product /> */}
+      {/* <Refrence /> */}
+      {/* <Care /> */}
+      {/* <Footer /> */}
+    </>
+  );
 }
 
 export default App;
- 
