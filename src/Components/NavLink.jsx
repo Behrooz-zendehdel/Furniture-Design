aa5a5072cd3e1493334d93bab0531a6a13eb5213
behrooz <behrooz.zendedel1992@gmail.com>
2023-11-28 22:04:29 +0330

    debuged navbar to hero components

--- src/Components/NavLink.jsx
+++ src/Components/NavLink.jsx
@@ -1,20 +1,21 @@
 // react-scroll
 import { Link } from "react-scroll";
-
 const NavLink = ({ children, to, offset = -50, mobileMenu = false }) => {
-  const className = mobileMenu 
-  ? 'text-lg text-black hover:text-white cursor-pointer hover:bg-[linear-gradiant(90deg,_#2ad598_0%,_#0093fd_100%)] w-full text-center'  
-  : 'text-lg lg:text-xl text-white cursor-pointer hover:bg-[linear-gradiant(90deg,_#2ad598_0%,_#0093fd_100%)] hover:bg-clip-text hover:text-transparent'
-   return (
-
-<Link to={to}
-  spy={true}
-  smooth={true}
-  offset={offset}
-  duration={1000}
-  className={className}
-></Link>
-  )
-   }
+  const className = mobileMenu
+    ? "text-lg text-black hover:text-white cursor-pointer hover:bg-[linear-gradient(90deg,_#2AF598_0%,_#009efd_100%)] w-full text-center"
+    : "text-lg lg:text-xl text-white cursor-pointer hover:bg-[linear-gradient(90deg,_#2af598_0%,_#009efd_100%)] hover:bg-clip-text hover:text-transparent";
+  return (
+    <Link
+      to={to}
+      spy={true}
+      smooth={true}
+      offset={offset}
+      duration={1000}
+      className={className}
+    >
+      {children}
+    </Link>
+  );
+};
 
 export default NavLink;
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -2,10 +2,14 @@ import NavLink from "../Components/NavLink";
 import heroImage from "../assets/hero-image.svg";
 import logo from "../assets/logo.svg";
 import carIcon from "../assets/cart-icon.svg";
-//react-iocons
-import { HiMenuAlt3 } from "@heroicons/react/24/solid"; // debuged code
+
+//react-icons
+import { HiMenuAlt3 } from "react-icons/hi"; // debuged code
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 const Hero = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   return (
     <div
       className="h-screen relative flex flex-col items-center "
@@ -31,7 +35,35 @@ const Hero = () => {
         <HiMenuAlt3
           size={30}
           className="block md:hidden cursor-pointer text-white"
+          onClick={() => setShowMobileMenu((prev) => !prev)}
         />
+        <div
+          className={`block md:hidden w-full fixed ${
+            !showMobileMenu ? "-top-[410px]" : "top-0"
+          } left-0 bg-[#dde0e5] h-[410px] transition-all duration-[800ms] shadow-xl z-10 py-8 px-12 rounded-b-xl`}
+        >
+          <AiOutlineClose
+            size="25"
+            className="absolute top-5 right-5 cursor-pointer"
+            onClick={() => setShowMobileMenu(false)}
+          />
+
+          <ul className="pt-[60px] items-center flex flex-col gap-8">
+            <NavLink to="services" mobileMenu>
+              Services
+            </NavLink>
+            <NavLink to="product" mobileMenu>
+              Shop
+            </NavLink>
+            <NavLink to="refrence" mobileMenu>
+              Refrence
+            </NavLink>
+            <NavLink to="care" mobileMenu>
+              Care
+            </NavLink>
+          </ul>
+          <img src={carIcon} className="mt-8 mx-auto cursor-pointer" alt="" />
+        </div>
       </div>
     </div>
   );
