ef8e7f9c5af05412156cb545e31964f83bba0b00
behrooz <behrooz.zendedel1992@gmail.com>
2023-12-05 23:06:14 +0330

    add input for search to hero component

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -2,6 +2,7 @@ import NavLink from "../Components/NavLink";
 import heroImage from "../assets/hero-image.svg";
 import logo from "../assets/logo.svg";
 import carIcon from "../assets/cart-icon.svg";
+import searchIcon from "../assets/search-icon.svg";
 //react-icons
 import { HiMenuAlt3 } from "react-icons/hi"; // debuged code
 import { AiOutlineClose } from "react-icons/ai";
@@ -73,6 +74,27 @@ const Hero = () => {
           Bring serenity to Your Pakce With Interior Plants
         </h1>
       </FadeIn>
+      <FadeIn delay={0.4} direction="down" padding fullWidth>
+        <h5 className="mt-6 text-center text-sm  xs:text-xl text-white max-w-[500px]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non,
+          temporibus?
+        </h5>
+      </FadeIn>
+      <FadeIn delay={0.2} direction="up" padding fullWidth>
+        <div className="relative w-full sx:w-[460px] mt-11 items-center justify-center flex">
+          <input
+            type=" text"
+            placeholder="search"
+            className="rounded-full w-full xs:w-[500px] xl:w-[700px] pl-6 pr-[68px] py-4  border-1 bg-gray-400 outline-none text-white text-base xs:text-lg placeholder-white "
+          />
+          <img
+            src={searchIcon}
+            alt=""
+            className="absolute top-2/4 -translate-y-2/4 right-3 h-11 w-11 cursor-pointer"
+          />
+        </div>
+      </FadeIn>
+      <div className="absolute h-[50px] xs:h-[150px] bottom-0 w-full bg-[linear-gradient(180deg,_#fffff00_0%,_#fff_100%)]"></div>
     </div>
   );
 };
